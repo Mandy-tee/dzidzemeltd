ce60f4f9ccb5bd1f1ab447acb3aede17cdf1ec57
Amanda Tagoe <nayork77@gmail.com>
2025-06-02 07:31:44 +0000

    Placeholder text color changed

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -49,7 +49,7 @@ const RegisterPage = () => {
                   name="firstName"
                   type="text"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-700 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
                   value={formData.firstName}
                   onChange={handleChange}
                 />
